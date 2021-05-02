d9abc05e796e72689b27f499a59bf18970770620
azinum <lukaska61@gmail.com>
2021-05-02 13:18:05 +0200

    Seperate effect functions into effect.c

---
+++ include/effect.h
@@ -0,0 +1,7 @@
+// effect.h
+
+static void Distortion(float* Buffer, i32 ChannelCount, i32 FramesPerBuffer, float Mix, float Amount);
+
+static void WeirdEffect(float* Buffer, i32 ChannelCount, i32 FramesPerBuffer, float Mix, float Amount);
+
+static void WeirdEffect2(float* Buffer, i32 ChannelCount, i32 FramesPerBuffer, float Mix, float Amount);
---
+++ src/effect.c
@@ -0,0 +1,54 @@
+// effect.c
+
+#define EFFECT_BUFFER_SIZE (1024 * 16)
+static float EffectBuffer[EFFECT_BUFFER_SIZE] = {0};
+static i32 EffectIndex = 0;
+static i32 CurrentEffectIndex = 0;
+
+void Distortion(float* Buffer, i32 ChannelCount, i32 FramesPerBuffer, float Mix, float Amount) {
+  float Dry = 1 - Mix;
+  float Wet = 1 - Dry;
+  float* Iter = Buffer;
+
+  for (i32 FrameIndex = 0; FrameIndex < FramesPerBuffer * ChannelCount; ++FrameIndex) {
+    float WetFrame = *Iter;
+    float DryFrame = WetFrame;
+    WetFrame *= Amount;
+    WetFrame = Clamp(WetFrame, -1.0f, 1.0f);
+    *(Iter++) = (DryFrame * Dry) + (WetFrame * Wet);
+  }
+}
+
+void WeirdEffect(float* Buffer, i32 ChannelCount, i32 FramesPerBuffer, float Mix, float Amount) {
+  float Dry = 1 - Mix;
+  float Wet = 1 - Dry;
+  float* Iter = Buffer;
+
+  for (i32 FrameIndex = 0; FrameIndex < FramesPerBuffer * ChannelCount; ++FrameIndex) {
+    float WetFrame = *Iter;
+    float DryFrame = WetFrame;
+
+    EffectBuffer[EffectIndex] = DryFrame;
+    EffectIndex = (EffectIndex + 1) % EFFECT_BUFFER_SIZE;
+    WetFrame = EffectBuffer[(EffectIndex & (i32)Amount) % EFFECT_BUFFER_SIZE];
+
+    *(Iter++) = (DryFrame * Dry) + (WetFrame * Wet);
+  }
+}
+
+void WeirdEffect2(float* Buffer, i32 ChannelCount, i32 FramesPerBuffer, float Mix, float Amount) {
+  float Dry = 1 - Mix;
+  float Wet = 1 - Dry;
+  float* Iter = Buffer;
+
+  for (i32 FrameIndex = 0; FrameIndex < FramesPerBuffer * ChannelCount; ++FrameIndex) {
+    float WetFrame = *Iter;
+    float DryFrame = WetFrame;
+
+    WetFrame = EffectBuffer[(i32)((CurrentEffectIndex ^ 0xb))];
+    CurrentEffectIndex = (CurrentEffectIndex + 1) % EFFECT_BUFFER_SIZE;
+
+    *(Iter++) = (DryFrame * Dry) + (WetFrame * Wet);
+  }
+}
+
